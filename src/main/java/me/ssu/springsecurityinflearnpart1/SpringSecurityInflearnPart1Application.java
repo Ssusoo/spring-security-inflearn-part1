05647e1b81edbc1d05fe181826e5c20735e19b92
김수환 <zzanggoon8@hiconsy.com>
2023-06-05 14:34:56 +0900

    Basic Setting

---
+++ src/main/java/me/ssu/springsecurityinflearnpart1/SpringSecurityInflearnPart1Application.java
@@ -0,0 +1,13 @@
+package me.ssu.springsecurityinflearnpart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInflearnPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityInflearnPart1Application.class, args);
+	}
+
+}
